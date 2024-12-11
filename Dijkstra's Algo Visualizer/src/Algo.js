7499630f6a8e898216e5aca2d5f1da71c9fc7237
Dinesh Kota <mail.kotadinesh@gmail.com>
2024-12-11 16:37:22 +0530

    Created Algo.js

---
+++ Dijkstra's Algo Visualizer/src/Algo.js
@@ -0,0 +1,160 @@
+export const places = [
+    "Delhi", "Agra", "Jaipur", "Mumbai", "Goa", "Chennai", "Bangalore", "Kolkata",
+    "Darjeeling", "Varanasi", "Hyderabad", "Pune", "Amritsar", "Shimla", "Manali",
+    "Leh", "Udaipur", "Rishikesh", "Haridwar", "Ooty", "Mysore", "Ahmedabad",
+    "Surat", "Lucknow", "Patna", "Guwahati", "Shillong", "Kochi", "Thiruvananthapuram",
+    "Jodhpur", "Ajmer", "Chandigarh", "Nagpur", "Indore", "Bhopal", "Raipur"
+];
+
+export const connections = [
+    { "from": "Delhi", "to": "Agra", "distance": 233 },
+    { "from": "Delhi", "to": "Jaipur", "distance": 268 },
+    { "from": "Delhi", "to": "Amritsar", "distance": 449 },
+    { "from": "Delhi", "to": "Shimla", "distance": 342 },
+    { "from": "Agra", "to": "Jaipur", "distance": 241 },
+    { "from": "Agra", "to": "Varanasi", "distance": 607 },
+    { "from": "Jaipur", "to": "Udaipur", "distance": 394 },
+    { "from": "Jaipur", "to": "Mumbai", "distance": 1148 },
+    { "from": "Mumbai", "to": "Pune", "distance": 149 },
+    { "from": "Mumbai", "to": "Goa", "distance": 583 },
+    { "from": "Goa", "to": "Bangalore", "distance": 556 },
+    { "from": "Bangalore", "to": "Chennai", "distance": 346 },
+    { "from": "Bangalore", "to": "Ooty", "distance": 265 },
+    { "from": "Bangalore", "to": "Mysore", "distance": 143 },
+    { "from": "Chennai", "to": "Kochi", "distance": 683 },
+    { "from": "Kochi", "to": "Thiruvananthapuram", "distance": 204 },
+    { "from": "Kolkata", "to": "Darjeeling", "distance": 634 },
+    { "from": "Kolkata", "to": "Guwahati", "distance": 983 },
+    { "from": "Guwahati", "to": "Shillong", "distance": 100 },
+    { "from": "Lucknow", "to": "Delhi", "distance": 554 },
+    { "from": "Lucknow", "to": "Varanasi", "distance": 320 },
+    { "from": "Patna", "to": "Varanasi", "distance": 284 },
+    { "from": "Rishikesh", "to": "Haridwar", "distance": 21 },
+    { "from": "Rishikesh", "to": "Shimla", "distance": 281 },
+    { "from": "Shimla", "to": "Manali", "distance": 248 },
+    { "from": "Manali", "to": "Leh", "distance": 479 },
+    { "from": "Ahmedabad", "to": "Udaipur", "distance": 256 },
+    { "from": "Ahmedabad", "to": "Mumbai", "distance": 526 },
+    { "from": "Ahmedabad", "to": "Indore", "distance": 401 },
+    { "from": "Indore", "to": "Bhopal", "distance": 194 },
+    { "from": "Bhopal", "to": "Nagpur", "distance": 354 },
+    { "from": "Nagpur", "to": "Raipur", "distance": 284 },
+    { "from": "Surat", "to": "Mumbai", "distance": 263 },
+    { "from": "Surat", "to": "Ahmedabad", "distance": 270 },
+    { "from": "Jodhpur", "to": "Ajmer", "distance": 203 },
+    { "from": "Jodhpur", "to": "Jaipur", "distance": 337 },
+    { "from": "Chandigarh", "to": "Shimla", "distance": 113 },
+    { "from": "Chandigarh", "to": "Amritsar", "distance": 229 },
+    { "from": "Delhi", "to": "Bangalore", "distance": 2160 },
+    { "from": "Delhi", "to": "Kolkata", "distance": 1516 },
+    { "from": "Delhi", "to": "Hyderabad", "distance": 1585 },
+    { "from": "Delhi", "to": "Pune", "distance": 1504 },
+    { "from": "Delhi", "to": "Chennai", "distance": 2190 },
+    { "from": "Mumbai", "to": "Bangalore", "distance": 981 },
+    { "from": "Mumbai", "to": "Kolkata", "distance": 1965 },
+    { "from": "Mumbai", "to": "Hyderabad", "distance": 660 },
+    { "from": "Mumbai", "to": "Chennai", "distance": 1334 },
+    { "from": "Goa", "to": "Hyderabad", "distance": 670 },
+    { "from": "Goa", "to": "Chennai", "distance": 765 },
+    { "from": "Goa", "to": "Kolkata", "distance": 1774 },
+    { "from": "Chennai", "to": "Hyderabad", "distance": 660 },
+    { "from": "Chennai", "to": "Kolkata", "distance": 1666 },
+    { "from": "Bangalore", "to": "Kolkata", "distance": 1745 },
+    { "from": "Bangalore", "to": "Hyderabad", "distance": 660 },
+    { "from": "Kochi", "to": "Hyderabad", "distance": 1093 },
+    { "from": "Kochi", "to": "Bangalore", "distance": 435 },
+    { "from": "Kochi", "to": "Kolkata", "distance": 1876 },
+    { "from": "Darjeeling", "to": "Kolkata", "distance": 634 },
+    { "from": "Varanasi", "to": "Bhopal", "distance": 826 },
+    { "from": "Varanasi", "to": "Mumbai", "distance": 1591 },
+    { "from": "Hyderabad", "to": "Pune", "distance": 520 },
+    { "from": "Hyderabad", "to": "Bangalore", "distance": 660 },
+    { "from": "Hyderabad", "to": "Chennai", "distance": 660 },
+    { "from": "Raipur", "to": "Nagpur", "distance": 284 },
+    { "from": "Raipur", "to": "Bhopal", "distance": 609 }
+];
+
+export let adjL = [];
+export let indices = new Map();
+export let revIndices = new Map();
+export let distances = []
+export let parent = []
+
+//Fuctions:
+const buildIndices = ()=>{
+    for(let i=0; i<places.length; i++) {
+        indices.set(places[i],i);
+        revIndices.set(i,places[i]);
+    }
+    // console.log(indices);
+    // console.log(revIndices);
+}
+
+const buildAdjL = ()=>{
+    adjL = Array.from({length:places.length},()=>[]);
+    connections.forEach((con)=>{
+        adjL[indices.get(con.from)].push(new Edge(indices.get(con.to),con.distance));
+        adjL[indices.get(con.to)].push(new Edge(indices.get(con.from),con.distance));
+    });
+    // console.log(adjL);
+}
+
+const buildDisatanceArr = (source)=>{
+    parent = Array.from({length: places.length},(_,index)=>index);
+    distances = new Array(places.length).fill(Infinity);
+    distances[source] = 0;
+    const pq = [new Pair(source,0)];
+    while(pq.length > 0) {
+        const curr = pq.shift();
+        adjL[curr.node].forEach((neg)=>{
+            if(neg.distance + curr.dist < distances[neg.v]) {
+                distances[neg.v] = neg.distance + curr.dist;
+                pq.push(new Pair(neg.v,distances[neg.v]));
+                parent[neg.v] = curr.node;
+            }
+        });
+    }
+    // console.log(parent);
+}
+
+export const getPath = (source,destination) =>{
+    buildDisatanceArr(indices.get(source));
+    let path = [];
+    let node = indices.get(destination);
+    path.push(revIndices.get(node));
+    while(node != parent[node]) {
+        path.push(revIndices.get(parent[node]));
+        node = parent[node];
+    }
+    path.reverse();
+    // path.forEach((i)=>{
+    //     console.log(revIndices.get(i));
+    // });
+    // console.log(path);
+    return path;
+}
+
+
+//Classes:
+class Edge {
+    constructor(v,distance) {
+        this.v = v;
+        this.distance = distance;
+    }
+}
+
+class Pair {
+    constructor(node,dist) {
+        this.node = node;
+        this.dist = dist;
+    }
+}
+
+//Function calls
+const main = ()=>{
+    buildIndices();
+    buildAdjL();
+    getPath("Delhi","Mumbai");
+}
+
+main();
